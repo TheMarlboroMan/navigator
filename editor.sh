f2ccabe56edadf0624bae68007b21aa9ea5e90be
Daniel Pastor <daniel.pastor@grupoancana.com>
2015-08-17 11:06:45 +0200

    Creadas tiles de template. Eliminado comando de editor, mejor usarlo desde otro directorio base.

--- editor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-if [ "$1" == "" ] || [ "$2" == "" ]; then
-	printf "./editor.sh #num_sala# #fichero_sala# :\n	ej: ./editor.sh 1 001\n (abrirá el fichero 01_001.dat)."
-	exit
-else 
-	../tile_editor/a.out res=640x480 cfg=integracion/navigator/config.dat out=data/salas/${1}/${1}_${2}.dat
-fi;
